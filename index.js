dba9778c8654a2fcabd2c0fb142c0dbb1b41be24
XPISigma <xpisigma@gmail.com>
2023-07-24 13:13:10 -0400

    script.js

--- index.js
+++ index.js
@@ -1,82 +1,43 @@
-const circles = document.querySelectorAll(".circle");
-const buttonNames = []; // Array to store button names
-
-circles.forEach((circle, index) => {
-  let isMouseDown = false;
-
-  // Function to display the name input box
-  function showNameInput() {
-    const name = prompt("Enter a name for the button:");
-    if (name) {
-      circle.innerHTML = name;
-      resizeFont(circle);
-      buttonNames[index] = name; // Save the name to the array
-      saveButtonNames(); // Save all button names to the cache
-    }
-  }
-
-  // Function to resize the font based on the button size
-  function resizeFont(button) {
-    const buttonSize = button.offsetWidth;
-    const fontSize = buttonSize * 0.15;
-    button.style.fontSize = `${fontSize}px`;
-  }
-
-  // Function to move the button down
-  function moveButtonDown() {
-    circle.style.transform = "translateY(20px)";
-  }
-
-  // Function to move the button back up
-  function moveButtonUp() {
-    circle.style.transform = "translateY(0)";
-  }
-
-  circle.addEventListener("mousedown", () => {
-    isMouseDown = true;
-    setTimeout(() => {
-      if (isMouseDown) {
-        showNameInput();
+  // Function to trigger vibration (if supported by the device)
+    function vibrateDevice() {
+      if ('vibrate' in navigator) {
+        // Vibrate for 50 milliseconds
+        navigator.vibrate(50);
       }
-    }, 3000);
+    }
 
-    moveButtonDown();
-  });
+    // Attach click events to buttons for desktop devices
+    const circles = document.querySelectorAll(".circle");
+    circles.forEach(circle => {
+      circle.addEventListener("mousedown", () => {
+        vibrateDevice(); // Vibrate when button is pressed
+        circle.style.top = "20px"; // Move the button down by 20px
+        console.log("Button Name:", circle.dataset.name);
+      });
 
-  circle.addEventListener("mouseup", () => {
-    isMouseDown = false;
-    moveButtonUp();
-  });
+      circle.addEventListener("mouseup", () => {
+        circle.style.top = "0"; // Move the button back up to its original position
+      });
+    });
 
-  circle.addEventListener("mouseout", () => {
-    isMouseDown = false;
-    moveButtonUp();
-  });
+    // Attach touch events to buttons for mobile devices
+    circles.forEach(circle => {
+      circle.addEventListener("touchstart", () => {
+        vibrateDevice(); // Vibrate when button is pressed
+        circle.style.top = "20px"; // Move the button down by 20px
+        console.log("Button Name:", circle.dataset.name);
+        playSound();
 
-  window.addEventListener("resize", () => {
-    resizeFont(circle);
-  });
+      });
 
-  resizeFont(circle);
+      circle.addEventListener("touchend", () => {
+        circle.style.top = "0"; // Move the button back up to its original position
+      });
+    });
 
-  // Retrieve the button name from the array and set it on the button
-  if (buttonNames[index]) {
-    circle.innerHTML = buttonNames[index];
-    resizeFont(circle); // Resize font after setting the text
-  }
-});
-
-// Function to save all button names to the cache
-function saveButtonNames() {
-  localStorage.setItem("buttonNames", JSON.stringify(buttonNames));
-}
-
-// Function to retrieve all button names from the cache
-function getButtonNames() {
-  const names = localStorage.getItem("buttonNames");
-  return names ? JSON.parse(names) : [];
-}
-
-// Retrieve button names from cache on page load
-buttonNames.push(...getButtonNames());
+function playSound() {
+      const audio = document.getElementById('myAudio');
+      audio.currentTime = 0; // Rewind the audio to the beginning to play it from the start each time
+      audio.play();
+    }
 
